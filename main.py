7c98da020f79208582d13e22cb7f896a469d976e
maikroservice <20245897+maikroservice@users.noreply.github.com>
2023-01-23 13:20:55 +0100

    add droplet listing

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 from dotenv import load_dotenv
 import discord
 from discord import Intents
-from twilio.rest import Client
+#from twilio.rest import Client
 from discord.ext import commands
 import os
 import requests
 
 load_dotenv()
 
-digitalocean_token = os.environ.get("digitalocean_token")
-discord_token = os.environ.get("discord_token")
-discord_allowed_role_id = os.environ.get("discord_allowed_role_id")
-digitalocean_droplet_id = os.environ.get("digitalocean_droplet_id")
+digitalocean_token = os.getenv("DIGITALOCEAN_TOKEN")
+discord_token = os.getenv("DISCORD_TOKEN")
+my_guild = os.getenv("DISCORD_GUILD")
+
+#discord_allowed_role_id = os.environ.get("discord_allowed_role_id")
+# digitalocean_droplet_id = os.environ.get("digitalocean_droplet_id")
+"""
 twilio_token = os.environ.get("twilio_token")
 twilio_account = os.environ.get("twilio_account")
 twilio_phone_number = os.environ.get("twilio_phone_number")
 user_phone_number = os.environ.get("user_phone_number")
-
+"""
 intents = Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
-twilio_client = Client(twilio_account, twilio_token)
+#twilio_client = Client(twilio_account, twilio_token)
 
 
 def reboot_digitalocean():
@@ -33,7 +36,16 @@ def reboot_digitalocean():
     else:
         print(response.status_code)
 
+def list_droplets():
+    endpoint = f'https://api.digitalocean.com/v2/droplets?page=1&per_page=10'
+    headers = {"Authorization": f"Bearer {digitalocean_token}", "Content-Type": "application/json"}
+    response = requests.get(endpoint, headers=headers)
+    if (response.status_code == 200):
+        return response.text[:1950]
+    else:
+        return response.status_code
 
+"""
 def text_message():
     message = twilio_client.messages \
         .create(
@@ -42,7 +54,7 @@ def text_message():
             to = user_phone_number 
         )
     return message.status
-
+"""
 
 @client.event
 async def on_message(message):
@@ -64,5 +76,23 @@ async def on_message(message):
         author_role_ids = [y.id for y in message.author.roles]
         if int(discord_allowed_role_id) in author_role_ids:
             await message.channel.send('I am awake...')
+
+    elif message.content.startswith('^list'):
+        droplets = list_droplets()
+        await message.channel.send(f"""```json
+{droplets}
+```""")
+        
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == my_guild:
+            break
+
+    print(
+        f"{client.user} is connected to the following guild:\n"
+        f"{guild.name}(id: {guild.id})"
+    )
 
 client.run(discord_token)
