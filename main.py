575886fdbe6ca4d5276df9c34b3be9abb107cf3b
maikroservice <20245897+maikroservice@users.noreply.github.com>
2023-01-25 14:44:19 +0100

    v1 output fix

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from discord import Intents
 from discord.ext import commands
 import os
 import requests
-
+import json
 load_dotenv()
 
 DIGITALOCEAN_TOKEN = os.getenv("DIGITALOCEAN_TOKEN")
@@ -25,11 +25,32 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 #twilio_client = Client(twilio_account, twilio_token)
 
+def prettify_droplet_list_output(droplet_dict):
+    droplet_message = """```
+  🧪📦 BlueTeam Lab Status 🧪📦
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+"""
+    
+    for droplet in droplet_dict['droplets']:
+        
+        d_id = droplet['id']
+        d_name = droplet['name']
+        d_location = droplet['region']['name'] 
+        d_status = droplet['status']
+        d_networks = droplet['networks']
+        d_ext_ip = d_networks["v4"][0]['ip_address']
+        d_int_ip = d_networks["v4"][1]['ip_address']
+
+        droplet_message += f"""{'🟢' if d_status == 'available' else '🟥'}{d_name}@{d_location} (id:{d_id})
+        🏠 ext:{d_ext_ip}, int:{d_int_ip}"""
+        droplet_message += "```"
+    return droplet_message
 
 def reboot_digitalocean():
     data = {"type": "reboot"}
     endpoint = f'https://api.digitalocean.com/v2/droplets/{digitalocean_droplet_id}/actions'
-    headers = {"Authorization": f"Bearer {digitalocean_token}"}
+    headers = {"Authorization": f"Bearer {DIGITALOCEAN_TOKEN}"}
     response = requests.post(endpoint, data=data, headers=headers)
     if (response.status_code == 201):
         print("Success")
@@ -38,10 +59,10 @@ def reboot_digitalocean():
 
 def list_droplets():
     endpoint = f'https://api.digitalocean.com/v2/droplets?page=1&per_page=10'
-    headers = {"Authorization": f"Bearer {digitalocean_token}", "Content-Type": "application/json"}
+    headers = {"Authorization": f"Bearer {DIGITALOCEAN_TOKEN}", "Content-Type": "application/json"}
     response = requests.get(endpoint, headers=headers)
     if (response.status_code == 200):
-        return response.text[:1950]
+        return response.json()
     else:
         return response.status_code
 
@@ -78,21 +99,23 @@ async def on_message(message):
             await message.channel.send('I am awake...')
 
     elif message.content.startswith('!list'):
-        droplets = list_droplets()
-        await message.channel.send(f"""```json
-{droplets}
-```""")
+        droplet_dict = list_droplets()
+        
+        droplet_message = prettify_droplet_list_output(droplet_dict)
+        await message.channel.send(droplet_message)
         
 
+
+"""
 @client.event
 async def on_ready():
     for guild in client.guilds:
-        if guild.name == my_guild:
+        if guild.name == DISCORD_GUILD:
             break
 
     print(
         f"{client.user} is connected to the following guild:\n"
         f"{guild.name}(id: {guild.id})"
     )
-
-client.run(discord_token)
+"""
+client.run(DISCORD_TOKEN)
