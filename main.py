12913c5d85d008c2311ec8c839c948af5b63440a
Maik Ro <20245897+maikroservice@users.noreply.github.com>
2023-01-27 23:53:50 +0100

    fix ip address formating for copy paste

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def prettify_droplet_list_output(droplet_dict):
         d_int_ip = d_networks["v4"][0]['ip_address']
 
         embed.add_field(name=f"{'🟢' if d_status == 'active' else '🟥'} **{d_name}**", value="", inline=False)
-        embed.add_field(name="", value=f"🏠 ext:{d_ext_ip}, int:{d_int_ip}", inline=False)
+        embed.add_field(name="", value=f"🏠 ext: `{d_ext_ip}`, int: `{d_int_ip}`", inline=False)
         embed.add_field(name=f"{f'▶️' if d_status != 'active' else f'⏹️'}", value=f"{f'`!start {d_id}`' if d_status != 'active' else f'`!stop {d_id}`'}", inline=False)
         embed.add_field(name=f"{f'♻️' if d_status == 'active' else ''}", value=f"{f'`!reboot {d_id}`' if d_status == 'active' else ''}", inline=False)
     embed.set_footer(text="Lab by @kazmsec + @maikroservice")
