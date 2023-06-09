c1bab9c1c1f4b1c9cd81c35d248c869b52edc397
maikroservice <20245897+maikroservice@users.noreply.github.com>
2023-06-10 00:25:22 +0200

    test

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import discord
 from discord import Intents
 from discord.ext import commands
+from discord.utils import get
 import os
 from verify_gumroad import verify_gumroad_license
 from digital_ocean import *
@@ -11,23 +12,49 @@ load_dotenv()
 DIGITALOCEAN_TOKEN = os.getenv("DIGITALOCEAN_TOKEN")
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 DISCORD_GUILD = os.getenv("DISCORD_GUILD")
+DISCORD_GUILD_ID = int(os.getenv("DISCORD_GUILD_ID"))
 GUMROAD_PRODUCT_ID = os.getenv("GUMROAD_PRODUCT_ID")
+DISCORD_SOC101_ROLE_ID = int(os.getenv("DISCORD_SOC101_ROLE_ID"))
 #DISCORD_ALLOWED_ROLE_ID = os.environ.get("DISCORD_ALLOWED_ROLE_ID")
 
 intents = Intents.default()
 intents.message_content = True
-client = discord.Client(intents=intents)
+#client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix="!", intents=intents)
+guild = bot.get_guild(DISCORD_GUILD_ID)
 
 
-@client.event
+@bot.event
+async def on_ready():
+    print("I am running on " + bot.user.name)
+    print("With the ID: " + str(bot.user.id))
+    print('Bot is ready to be used')
+   # after it is ready do it
+
+    takenGuild = bot.get_guild(DISCORD_GUILD_ID)
+    print(takenGuild.id)
+
+    for guild in bot.guilds:
+        print(guild)
+        print(guild.id)
+        for role in guild.roles:
+            print(role, role.id)
+
+
+@bot.event
+#@bot.command(pass_context=True)
 async def on_message(message):
 
     if message.content.startswith('!verify'):
         _, gumroad_key = message.content.split(" ")
         gumroad_key_verified = verify_gumroad_license(gumroad_key)
-        server = bot.get_guild(message.guild.id)
-        #if gumroad_key_verified:
+        
+        
+        if gumroad_key_verified:
+            #role = get(message.server.roles, name='SOC Analyst 101')
+            soc101 = await guild.get_role(DISCORD_SOC101_ROLE_ID) 
+            await bot.add_roles(message.author, soc101)
+
             # https://docs.replit.com/tutorials/python/discord-role-bot
         # add user to SOC Analyst 101 Discord Group
         # TODO: read "group" from verification product
@@ -39,7 +66,7 @@ async def on_message(message):
             
 
 
-        await message.channel.send(f'`Verification - {gumroad_key_verified} - server:{server}` ')
+        await message.channel.send(f'`Verification - {gumroad_key_verified}` ')
 
     if message.content.startswith('!reboot'):
         _, droplet_id = message.content.split(" ")
@@ -74,4 +101,4 @@ async def on_message(message):
         droplet_message = prettify_droplet_list_output(droplet_dict)
         await message.channel.send(embed=droplet_message)
         
-client.run(DISCORD_TOKEN)
+bot.run(DISCORD_TOKEN)
