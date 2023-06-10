28a6ec7b0b3f9ecd31898883280845601b2d504a
maikroservice <20245897+maikroservice@users.noreply.github.com>
2023-06-10 23:13:00 +0200

    test loggin

--- verify_gumroad.py
+++ verify_gumroad.py
@@ -4,7 +4,9 @@ def verify_gumroad_license(GUMROAD_PRODUCT_ID, license_key):
     r = requests.post("https://api.gumroad.com/v2/licenses/verify",
                       data={"product_id":GUMROAD_PRODUCT_ID, 
                             "license_key":license_key}).json()
+    print(r)
     if(r["uses"] > 1): 
        return "Key already used - contact @maikroservice"
-    
+    elif not r["uses"]:
+        return "Something went Wrong"
     return r["success"] and not r["purchase"]["refunded"]
