1fe8f6db400f845a080881608b8f7a79df5eb032
maikroservice <20245897+maikroservice@users.noreply.github.com>
2023-06-13 22:15:36 +0200

    add datefmt

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ import sys
 
 logging.basicConfig(stream=sys.stdout, 
                     level=logging.INFO,
-                        format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+                    format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
 )
 
 load_dotenv()
